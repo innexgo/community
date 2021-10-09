1b5ca43094734cc9a055cc3464f32afcef6e6cba
mbajji <43502655+mbajji@users.noreply.github.com>
2021-10-09 13:32:06 -0700

    update 10/9/2021

--- src/pages/stories/Story4.tsx
+++ src/pages/stories/Story4.tsx
@@ -6,7 +6,7 @@ import image from '../../assets/story4/story4.jpg';
 const CMStory4 = () => <Layout>
   <div id="images" className="container pt-5" >
     <Story
-      title="Be A Saint Promotional Mural"
+      title="Kids saved from overheating car"
       blurb="These staff members demonstrated their CAN-DO ATTITUDE when they discovered a woman who had accidentally locked her kids and keys in the car.  The temperature was 90+ so they had to act fast.  The team had the where-with-all to cover the car with shade and keep the kids calm while they found a hammer to smash the window, saving them from the heat. "
       staff={["Mr. O'Brien", "Mr. Papin", "Ms. Plemel", "Mr. Oxtot", "Mr. Pham"]}
       date="September 2021"
